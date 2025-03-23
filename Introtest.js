9df82e66952f612bb818ef6706d12a3e688c19e8
Ogeconstance <161642104+Ogeconstance@users.noreply.github.com>
2025-03-23 02:59:05 +0100

    Initial commit

---
+++ Introtest.js
@@ -0,0 +1,47 @@
+const change = document.getElementById("imgname")
+change.addEventListener("mouseenter", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("imgname2")
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("imgname3")
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("imgname4")
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("imgname5")
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("imgname6")
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("imgname7")
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
+
+const change8 = document.getElementById("imgname8")
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "cadetblue";
+    event.target.style.color = "white";
+});
